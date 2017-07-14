0da465df43e71da9905c3d7cb87a0ee31adbbb71
maherm <martin.maher@consol.de>
2017-07-14 17:54:28 +0200

    fixed bug: the interceptor was being registered multiple times for the same endpoint

--- simulator-core/src/main/java/com/consol/citrus/simulator/annotation/SimulatorWebServiceAdapter.java
+++ simulator-core/src/main/java/com/consol/citrus/simulator/annotation/SimulatorWebServiceAdapter.java
@@ -18,8 +18,6 @@ package com.consol.citrus.simulator.annotation;
 
 import com.consol.citrus.endpoint.adapter.mapping.MappingKeyExtractor;
 import com.consol.citrus.endpoint.adapter.mapping.XPathPayloadMappingKeyExtractor;
-import com.consol.citrus.ws.interceptor.LoggingEndpointInterceptor;
-import org.springframework.ws.server.EndpointInterceptor;
 
 /**
  * @author Christoph Deppisch
@@ -36,8 +34,4 @@ public abstract class SimulatorWebServiceAdapter implements SimulatorWebServiceC
         return new XPathPayloadMappingKeyExtractor();
     }
 
-    @Override
-    public EndpointInterceptor[] interceptors() {
-        return new EndpointInterceptor[]{new LoggingEndpointInterceptor()};
-    }
 }
