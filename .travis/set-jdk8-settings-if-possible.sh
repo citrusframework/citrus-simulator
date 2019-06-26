6bd41c1deb2a348d4f6dcee2f6e1f48c0376b6d3
Timon Borter <timon.borter.1@postfinance.ch>
2019-06-26 08:42:14 +0200

    replace javax with jakarta dependencies, fix travis setup for jdk11.

--- .travis/set-jdk8-settings-if-possible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copies jdk8 settings.xml (Maven) to $HOME if Java version matches
-
-if [[ $JAVA_HOME =~ "java-8-oracle" ]]
-then
-  cp ./.travis/jdk8-settings.xml $HOME/.m2/settings.xml
-fi
