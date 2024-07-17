6b95f2aa671534c0a0b0c04866e15aafea9a9cf3
calusicle <leon.calusic.1@postfinance.ch>
2024-07-17 14:58:09 +0200

    feat(simulator-ui): integrate playwright

--- simulator-ui/tests/customExample.spec.ts
+++ simulator-ui/tests/customExample.spec.ts
@@ -16,3 +16,13 @@ test('clicking Details button', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByText('Scenario Executions')).toBeVisible();
 });
+
+test('no', async ({ page }) => {
+  await page.goto('http://localhost:9000/');
+
+  // Click the get started link.
+  await page.locator('button:text("Details")');
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByText('Scenario Executions')).toBeVisible();
+});
