8e336e536831b634591ff15d044f49f88acbbb9a
Andy Huynh <andy.huynh.2@postfinance.ch>
2024-08-12 12:13:45 +0200

    test(simulator-ui): add test for table row and input form

--- simulator-ui/tests/customExample.spec.ts
+++ simulator-ui/tests/customExample.spec.ts
@@ -28,19 +28,19 @@ test('should have total, successful, failed tabs', async ({page}) => {
 
 test('total, successful, failed tabs should display simulations count', async ({page}) => {
   await page.route('**/api/test-results/count-by-status', async route => {
-    const json = {"successful": 746039, "failed": 490, "total": 746529};
+    const json = {"successful": 170, "failed": 30, "total": 200};
     await route.fulfill({json});
   });
 
   await page.goto('http://localhost:9000/');
 
-  await expect(page.getByText('746529')).toBeVisible();
-  await expect(page.getByText('746039')).toBeVisible();
-  await expect(page.getByText('490')).toBeVisible();
+  await expect(page.getByText('200')).toBeVisible();
+  await expect(page.getByText('170')).toBeVisible();
+  await expect(page.getByText('30')).toBeVisible();
 });
 
 test('total, successful, failed tabs should display percentage in simulations count', async ({page}) => {
-  await page.route('*/**/api/test-results/count-by-status', async route => {
+  await page.route('**/api/test-results/count-by-status', async route => {
     const json = {"successful": 746039, "failed": 490, "total": 746529};
     await route.fulfill({json});
   });
@@ -52,19 +52,29 @@ test('total, successful, failed tabs should display percentage in simulations co
   await expect(page.getByText('0.07 %')).toBeVisible();
 });
 
-test('should click Details button', async ({page}) => {
+test('should display footer', async ({page}) => {
+  await page.goto('http://localhost:9000/');
+
+  await expect(page.getByText('©2023 to the original author or authors.')).toBeVisible();
+  await expect(page.getByText('Citrusframework/Simulator')).toBeVisible();
+  await expect(page.getByText('Find us on:')).toBeVisible();
+});
+
+test('should display input form', async ({page}) => {
   await page.goto('http://localhost:9000/scenario-result/');
 
   await expect(page.getByText('Items per Page')).toBeVisible();
+  await expect(page.locator('#pageSize')).toHaveValue('10');
+  await expect(page.getByLabel('Scenario Name')).toBeVisible();
 });
 
-test('should display list categories', async ({page}) => {
-  await page.route('*/**/api/scenario-executions*', async route => {
-    const json = [
+test('should display table headers for scenario executions', async ({page}) => {
+  await page.route('**/api/scenario-executions*', async route => {
+    const scenarioExecutionJson = [
       {
         "executionId": 752603,
         "startDate": "2024-06-27T10:59:03.872021Z",
-        "endDate": "2024-06-27T10:59:04.168103Z",
+        "endDate": "2024-06-27T11:05:21.168103Z",
         "scenarioName": "Default",
         "testResult": {
           "id": 752026,
@@ -74,23 +84,50 @@ test('should display list categories', async ({page}) => {
         },
       }
     ];
-    await route.fulfill({json});
+    await route.fulfill({json: scenarioExecutionJson});
   });
 
   await page.goto('http://localhost:9000/scenario-result/');
 
-  await expect(page.locator('table th :text("ID")')).toHaveCount(1);
-  await expect(page.locator('table th :text("Scenario Name")')).toHaveCount(1);
-  await expect(page.locator('table th :text("Start Date")')).toHaveCount(1);
-  await expect(page.locator('table th :text("End Date")')).toHaveCount(1);
-  await expect(page.locator('table th :text("Status")')).toHaveCount(1);
-  await expect(page.locator('table th :text("Error Message")')).toHaveCount(1);
+  await expect(page.locator('th :text("ID")')).toHaveCount(1);
+  await expect(page.locator('th :text("Scenario Name")')).toHaveCount(1);
+  await expect(page.locator('th :text("Start Date")')).toHaveCount(1);
+  await expect(page.locator('th :text("End Date")')).toHaveCount(1);
+  await expect(page.locator('th :text("Status")')).toHaveCount(1);
+  await expect(page.locator('th :text("Error Message")')).toHaveCount(1);
 });
 
-test('should display footer', async ({page}) => {
-  await page.goto('http://localhost:9000/');
+test('should display table row for scenario executions', async ({page}) => {
+  await page.route('**/api/scenario-executions*', async route => {
+    const scenarioExecutionJson = [
+      {
+        "executionId": 752603,
+        "startDate": "2024-06-27T10:59:03.872021Z",
+        "endDate": "2024-06-27T11:05:21.168103Z",
+        "scenarioName": "Default",
+        "testResult": {
+          "id": 752026,
+          "status": "FAILURE",
+          "errorMessage": "Unable to validate because message store is not of type 'CorrelatedMessageProvider'! Check your configuration and register a suitable message store.",
+          "createdDate": "2024-06-27T08:59:04.159168Z",
+        },
+      }
+    ];
+    await route.fulfill({json: scenarioExecutionJson});
+  });
 
-  await expect(page.getByText('©2023 to the original author or authors.')).toBeVisible();
-  await expect(page.getByText('Citrusframework/Simulator')).toBeVisible();
-  await expect(page.getByText('Find us on:')).toBeVisible();
+  await page.goto('http://localhost:9000/scenario-result/');
+
+  await expect(page.locator('tr :text("752603")')).toHaveCount(1);
+  await expect(page.locator('tr :text("Default")')).toHaveCount(1);
+  await expect(page.locator('tr :text("27 Jun 2024 10:59:03")')).toHaveCount(1);
+  await expect(page.locator('tr :text("27 Jun 2024 11:05:21")')).toHaveCount(1);
+  await expect(page.locator('tr :text("FAILURE")')).toHaveCount(1);
+  await expect(page.locator('tr :text("Unable to validate because message store is not of type \'CorrelatedMessageProvider\'! Check your configuration and register a suitable message store.")')).toHaveCount(1);
+  await expect(page.locator('tr :text("Scenario Actions")')).toHaveCount(1);
+  await expect(page.locator('tr :text("Scenario Messages")')).toHaveCount(1);
+  await expect(page.locator('tr :text("Scenario Parameters")')).toHaveCount(1);
+  await expect(page.locator('tr :text("View")')).toHaveCount(1);
 });
+
+
