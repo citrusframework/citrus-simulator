59d3125bb2ef6253f931f4df3101cb206ff4b387
Andy Huynh <andy.huynh.2@postfinance.ch>
2024-08-15 14:30:08 +0200

    test(simulator-ui): fix tests with links and new tabs

--- simulator-ui/tests/home.spec.ts
+++ simulator-ui/tests/home.spec.ts
@@ -1,12 +1,12 @@
 import {expect, Page, test} from '@playwright/test';
 
-let successfulTests = 90;
-let failedTests = 10;
-let totalTests = successfulTests + failedTests;
+let successfulTestsMock = 90;
+let failedTestsMock = 10;
+let totalTestsMock = successfulTestsMock + failedTestsMock;
 
 test.beforeEach(async ({page}) => {
   await page.route('**/api/test-results/count-by-status', async route => {
-    const json = {"successful": successfulTests, "failed": failedTests, "total": totalTests};
+    const json = {"successful": successfulTestsMock, "failed": failedTestsMock, "total": totalTestsMock};
     await route.fulfill({json});
   });
   await page.goto('http://localhost:9000/');
@@ -27,8 +27,7 @@ test('should have total, successful, failed tabs', async ({page}) => {
   await expect(page.getByTestId('home/successfulTestsSummary')).toBeVisible();
   await expect(page.getByTestId('home/failedTestsSummary')).toBeVisible();
 
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
-
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 });
 
 test('total, successful, failed tabs should display percentage in simulations count', async ({page}) => {
@@ -55,11 +54,18 @@ test('should move to right page with Github link', async ({page}) => {
   await newTab.waitForLoadState();
 
 // Interact with the new page normally.
-  await expect(page).toHaveURL('https://github.com/citrusframework/citrus-simulator/');
+  await expect(newTab).toHaveURL('https://github.com/citrusframework/citrus-simulator');
 })
 test('should move to right page with citrus link', async ({page}) => {
-  await page.getByTestId('home/feedbackAndSuggestionLink').click();
-  await expect(page).toHaveURL('https://github.com/citrusframework/citrus-simulator/issues/new');
+  const [newTab] = await Promise.all([
+    // Start waiting for new page before clicking. Note no await.
+    page.waitForEvent("popup"),
+    page.getByTestId('home/feedbackAndSuggestionLink').click()
+  ]);
+
+  await newTab.waitForLoadState();
+
+  await expect(newTab).toHaveURL(RegExp('https://github.com*'));
 })
 
 test('should move to scenario-results page with right search field params after click on total-details', async ({page}) => {
@@ -84,35 +90,35 @@ test('should move to scenario-results page with right search field params after 
 })
 
 test('should have updated total, successful, failed tabs after refresh button clicked positive test', async ({page}) => {
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 
-  failedTests -= 10;
-  successfulTests += 10;
+  failedTestsMock -= 10;
+  successfulTestsMock += 10;
   await page.route('**/api/test-results/count-by-status', async route => {
-    const json = {"successful": successfulTests, "failed": failedTests, "total": totalTests};
+    const json = {"successful": successfulTestsMock, "failed": failedTestsMock, "total": totalTestsMock};
     await route.fulfill({json});
   });
   await page.getByTestId('home/refreshListButton').click();
 
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 })
 
 test('should have updated total, successful, failed tabs after refresh button clicked negative test', async ({page}) => {
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 
-  failedTests -= 10; // so the Total will be wrong!
-  const newCorrectTotal: number = totalTests - 10;
+  failedTestsMock -= 10; // so the Total will be wrong!
+  const newCorrectTotal: number = totalTestsMock - 10;
   await page.route('**/api/test-results/count-by-status', async route => {
-    const json = {"successful": successfulTests, "failed": failedTests, "total": totalTests};
+    const json = {"successful": successfulTestsMock, "failed": failedTestsMock, "total": totalTestsMock};
     await route.fulfill({json});
   });
   await page.getByTestId('home/refreshListButton').click();
 
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, newCorrectTotal, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, newCorrectTotal, successfulTestsMock, failedTestsMock);
 })
 
 test('should have same total, successful, failed tabs after cancel Deletion via cross-Button', async ({page}) => {
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 
   await page.getByTestId('home/resetButton').click();
   await expect(page.getByTestId('home/deletePopup')).toBeVisible();
@@ -120,11 +126,11 @@ test('should have same total, successful, failed tabs after cancel Deletion via 
   // HOW assert that API was NOT called?
   await expect(page.getByTestId('home/deletePopup')).toBeHidden();
 
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 })
 
 test('should have same total, successful, failed tabs after cancel Deletion via cancel-Button', async ({page}) => {
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 
   await page.getByTestId('home/resetButton').click();
   await expect(page.getByTestId('home/deletePopup')).toBeVisible();
@@ -132,21 +138,21 @@ test('should have same total, successful, failed tabs after cancel Deletion via 
   // HOW assert that API was NOT called?
   await expect(page.getByTestId('home/deletePopup')).toBeHidden();
 
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 })
 
 test('should have reset total, successful, failed tabs after confirmed Deletion with (200, OK) response', async ({page}) => {
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 
   await page.getByTestId('home/resetButton').click();
   await expect(page.getByTestId('home/deletePopup')).toBeVisible();
 
-  successfulTests = 0;
-  failedTests = 0;
-  totalTests = 0;
+  successfulTestsMock = 0;
+  failedTestsMock = 0;
+  totalTestsMock = 0;
 
   await page.route('**/api/test-results/count-by-status', async route => {
-    const json = {"successful": successfulTests, "failed": failedTests, "total": totalTests};
+    const json = {"successful": successfulTestsMock, "failed": failedTestsMock, "total": totalTestsMock};
     await route.fulfill({json});
   })
   const deleteRequestPromise = page.waitForRequest(request =>
@@ -164,10 +170,10 @@ test('should have reset total, successful, failed tabs after confirmed Deletion 
 
   await expect(page.getByTestId('home/deletePopup')).toBeHidden();
 
-  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTests, successfulTests, failedTests);
+  await checkIfSummaryTabsAreDisplayingRightNumbersFunction(page, totalTestsMock, successfulTestsMock, failedTestsMock);
 })
 
-const checkIfSummaryTabsAreDisplayingRightNumbersFunction = async (page: Page, totalTests: number, successfulTests: number, failedTests: number) => {
+const checkIfSummaryTabsAreDisplayingRightNumbersFunction = async (page: Page, totalTests: number, successfulTests: number, failedTests: number): Promise<any> => {
   await expect(page.getByTestId('home/totalTestsNumber')).toHaveText(totalTests + ` (${(totalTests / totalTests * 100).toLocaleString(undefined, {
     maximumFractionDigits: 2,
     minimumFractionDigits: 0
