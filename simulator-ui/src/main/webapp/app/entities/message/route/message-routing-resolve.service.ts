be518b564e138bbef4e8f66bcab2b4d12eb4c781
Timon Borter <timon.borter@gmx.ch>
2024-08-19 18:30:44 +0200

    feat(simulator-ui): code highlighting in message payloads

--- simulator-ui/src/main/webapp/app/entities/message/route/message-routing-resolve.service.ts
+++ simulator-ui/src/main/webapp/app/entities/message/route/message-routing-resolve.service.ts
@@ -1,28 +1,44 @@
-import { inject } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, Router } from '@angular/router';
-import { of, EMPTY, Observable } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
 
-import { IMessage } from '../message.model';
-import { MessageService } from '../service/message.service';
+import { of, EMPTY, Observable, from } from 'rxjs';
+import { map, mergeMap, switchMap } from 'rxjs/operators';
+
+import { IMessage } from 'app/entities/message/message.model';
+import { MessageService } from 'app/entities/message/service/message.service';
+import { CodeFormatterService } from 'app/shared/code-formatter.service';
 
 export const messageResolve = (route: ActivatedRouteSnapshot): Observable<null | IMessage> => {
+  const messageService = inject(MessageService);
+  const codeFormatterService = inject(CodeFormatterService);
+
   const messageId = route.params['messageId'];
+
   if (messageId) {
-    return inject(MessageService)
-      .find(messageId)
-      .pipe(
-        mergeMap((message: HttpResponse<IMessage>) => {
-          if (message.body) {
-            return of(message.body);
+    return messageService.find(messageId).pipe(
+      mergeMap((response: HttpResponse<IMessage>) => {
+        const message = response.body;
+
+        if (message) {
+          if (message.payload) {
+            return codeFormatterService.formatCode(message.payload).pipe(
+              map(result => {
+                message.payload = result;
+                return message;
+              }),
+            );
           } else {
-            inject(Router).navigate(['404']);
-            return EMPTY;
+            return of(message);
           }
-        }),
-      );
+        } else {
+          inject(Router).navigate(['404']);
+          return EMPTY;
+        }
+      }),
+    );
   }
+
   return of(null);
 };
 
