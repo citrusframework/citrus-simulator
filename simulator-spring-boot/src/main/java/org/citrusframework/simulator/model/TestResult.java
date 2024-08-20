22b4ddf71f2c0d11b3dfb1115b4ed93cea9ffa88
Melvin Johner <melvin.johner@postfinance.ch>
2024-08-20 11:53:25 +0200

    fix(#281): cascade delete on TestResult to ScenarioExecution

--- simulator-spring-boot/src/main/java/org/citrusframework/simulator/model/TestResult.java
+++ simulator-spring-boot/src/main/java/org/citrusframework/simulator/model/TestResult.java
@@ -40,6 +40,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.CascadeType.REMOVE;
 import static java.util.Objects.nonNull;
 import static lombok.AccessLevel.NONE;
 import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCause;
@@ -132,7 +133,7 @@ public class TestResult extends AbstractAuditingEntity<TestResult, Long> impleme
     private String failureType;
 
     @ToString.Exclude
-    @OneToOne(mappedBy = "testResult")
+    @OneToOne(mappedBy = "testResult", cascade = {REMOVE})
     private ScenarioExecution scenarioExecution;
 
     /**
