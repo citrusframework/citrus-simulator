67db6e56485c2c373bbbe02f3366c12b08c95e80
muellerfluri <flurina.mueller.2@postfinance>
2024-08-22 11:20:33 +0200

    test(citrus-simulator-ui): complete navbar tests

--- simulator-ui/tests/navbar.spec.ts
+++ simulator-ui/tests/navbar.spec.ts
@@ -1,20 +1,58 @@
 import {expect, test} from "@playwright/test";
-
+const elementLinkPairNoDropdown = [
+  {'testName': 'navigationScenariosLink', 'link': /.*scenario*/},
+  {'testName': 'navigationScenarioExecutionsLink', 'link': /.*scenario-result*/}
+  ]
+const elementLinkPairEntityDroptdown = [
+  {'testName': 'navigationEntitiesMessageLink', 'link': /.*\/message*/},
+  {'testName': 'navigationEntitiesMessageHeaderLink', 'link': /.*\/message-header*/},
+  {'testName': 'navigationEntitiesScenarioExecutionLink', 'link': /.*\/scenario-execution*/},
+  {'testName': 'navigationEntitiesScenarioActionLink', 'link': /.*\/scenario-action*/},
+  {'testName': 'navigationEntitiesScenarioParameterLink', 'link': /.*\/scenario-parameter*/},
+  {'testName': 'navigationEntitiesTestResultLink', 'link': /.*\/test-result*/},
+  {'testName': 'navigationEntitiesParameterLink', 'link': /.*\/test-parameter*/},
+]
 test.beforeEach(async ({page}) => {
   await page.goto('http://localhost:9000/');
 })
 
-test('should move to scenario page when navigating to scenario', async ({page}) => {
-  await page.getByTestId('navigationScenariosLink').click();
-  await expect(page).toHaveURL(/.*scenario*/);
+test('should move to scenario page when navigating to scenario resp. scenario-result', async ({page}) => {
+  for (const element of elementLinkPairNoDropdown) {
+    await page.getByTestId(element.testName).click();
+    await expect(page).toHaveURL(element.link);
+  }
 })
-test('should move to scenario-result when navigating to scenario executions', async ({page}) => {
-  await page.getByTestId('navigationScenarioExecutionsLink').click();
-  await expect(page).toHaveURL(/.*scenario-result*/);
-})
+
 test('should move to scenario-result and then move back home ', async ({page}) => {
   await page.getByTestId('navigationScenarioExecutionsLink').click();
   await expect(page).toHaveURL(/.*scenario-result*/);
   await page.getByTestId('navigationHomeLink').click();
-  await expect(page).toHaveURL(/.*\//); //TODO: does this work properly?
+  await expect(page).toHaveURL(/.*\//);
 })
+test('should move to all entities pages as intended', async ({page}) => {
+  for (const element of elementLinkPairEntityDroptdown) {
+    await page.getByTestId('navigationEntitiesLink').click();
+    await page.getByTestId(element.testName).click();
+    await expect(page).toHaveURL(element.link);
+  }
+})
+
+test('should move to the documentation in a new Tab', async ({page}) => {
+  const [newTab] = await Promise.all([
+    // Start waiting for new page before clicking. Note no await.
+    page.waitForEvent("popup"),
+
+    page.getByTestId('entity').click(),
+    page.getByTestId('documentation').click()
+  ]);
+  await newTab.waitForLoadState();
+
+  // Interact with the new page normally.
+  await expect(newTab).toHaveURL(/.*\/\/citrusframework.org\/citrus-simulator/);
+})
+test('should move to the swaggerUI documentation', async ({page}) => {
+  await page.getByTestId('entity').click();
+  await page.getByTestId('swaggerUI').click();
+  await expect(page).toHaveURL(/.*swagger-ui\/index.html/);
+})
+
