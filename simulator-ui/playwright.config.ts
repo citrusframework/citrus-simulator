04a691a5186e6f94aea40b4990d17a57b7f39fde
muellerfluri <flurina.mueller.2@postfinance>
2024-09-04 15:12:12 +0200

    test(citrus-simulator-ui): change port for playwright webServer and regenerate package-lock

--- simulator-ui/playwright.config.ts
+++ simulator-ui/playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://127.0.0.1:9000',
     reuseExistingServer: !process.env.CI,
   },
 });
